9e1a94b3de7febadfe8ba62e99dbbb0a2166399c
Lavanya Sood <lavanyasood09@gmail.com>
2023-03-31 23:29:39 +1100

    Added working calendar strip

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -1,10 +1,26 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
+import CalendarStrip from "react-native-calendar-strip";
+import moment from "moment";
 
 export default function HomePage() {
   const dispatch = useDispatch();
+  // let datesWhitelist = [
+  //   {
+  //     start: moment(),
+  //     end: moment().add(3, "days"), // total 4 days enabled
+  //   },
+  // ];
+  const startDate = moment().subtract(1, "days");
+  let datesBlacklist = [
+    {
+      start: moment().add(1, "days"),
+      end: moment().add(1, "years"), // total 4 days enabled
+    },
+  ];
   const data = useSelector((state) => state);
+  const today = moment();
   console.log("DATA");
   console.log(data);
   return (
@@ -14,6 +30,68 @@ export default function HomePage() {
       <View style={styles.parent}>
         <View style={styles.child}>
           <Text>Today</Text>
+          <Text>Today</Text>
+          <Text>Today</Text>
+          {/* <CalendarStrip
+            scrollable
+            calendarAnimation={{ type: "sequence", duration: 30 }}
+            daySelectionAnimation={{
+              type: "border",
+              duration: 200,
+              borderWidth: 1,
+              borderHighlightColor: "white",
+            }}
+            style={{ height: 100 }}
+            // calendarHeaderStyle={{ color: "white" }}
+            showMonth={false}
+            calendarColor={"#7743CE"}
+            dateNumberStyle={{ color: "white", fontSize: 30 }}
+            dateNameStyle={{ color: "white", fontSize: 15 }}
+            highlightDateNumberStyle={{ color: "white", fontSize: 30 }}
+            highlightDateNameStyle={{ color: "yellow" }}
+            disabledDateNameStyle={{ color: "grey" }}
+            disabledDateNumberStyle={{ color: "grey" }}
+            iconContainer={{ flex: 0.1 }}
+            responsiveSizingOffset={23} */}
+          {/* /> */}
+          <CalendarStrip
+            calendarAnimation={{ type: "sequence", duration: 10 }}
+            daySelectionAnimation={{
+              type: "background",
+              duration: 200,
+              borderWidth: 1,
+              borderHighlightColor: "#3fadc4",
+              highlightColor: "#3fadc4",
+            }}
+            showMonth={false}
+            style={{ height: 100, paddingTop: 10, paddingBottom: 10 }}
+            calendarHeaderStyle={{ color: "black" }}
+            dateNumberStyle={{
+              color: "black",
+              fontSize: 20,
+              fontFamily: "Nunito-Sans-Bold",
+            }}
+            dateNameStyle={{
+              color: "black",
+              fontSize: 12,
+              fontFamily: "Nunito-Sans",
+            }}
+            startingDate={today}
+            selectedDate={today}
+            highlightDateNumberStyle={{
+              color: "#fff",
+              fontSize: 20,
+              fontFamily: "Nunito-Sans-Bold",
+            }}
+            highlightDateNameStyle={{
+              color: "#fff",
+              fontSize: 12,
+              fontFamily: "Nunito-Sans",
+            }}
+            // datesWhitelist={datesWhitelist}
+            datesBlacklist={datesBlacklist}
+            iconContainer={{ flex: 0.1 }}
+          />
         </View>
       </View>
       <ScrollView>
@@ -31,7 +109,8 @@ export default function HomePage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
+    // alignItems: "center",
+    width: "100%",
     // justifyContent: "center",
   },
   parent: {
@@ -47,7 +126,8 @@ const styles = StyleSheet.create({
     transform: [{ scaleX: 0.5 }],
 
     backgroundColor: "#ffffff",
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
+    width: "100%",
   },
 });
